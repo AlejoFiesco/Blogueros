f13fcaae765f30ee124f0bde0d68443a4d0b1251
adelquis-molinari <adelkis003@gmail.com>
2022-09-20 19:28:39 -0300

    fix on app startup, implementation of routes and animation

---
+++ src/layout/motion-route.js
@@ -0,0 +1,14 @@
+import { motion } from "framer-motion";
+
+const MotionRoute = ({ children }) => (
+  <motion.div
+    initial={{ opacity: 0 }}
+    animate={{ opacity: 1 }}
+    exit={{ opacity: 0 }}
+    transition={{ duration: 1 }}
+  >
+    {children}
+  </motion.div>
+);
+
+export default MotionRoute;
