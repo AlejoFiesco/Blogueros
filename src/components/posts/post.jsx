8bfa347dc33239e39041fd8dec6a0cd67d3c0e4a
AlejoFiesco <alfiesparra@hotmail.com>
2022-09-23 11:58:29 -0500

    Posts added

--- src/components/posts/post.jsx
+++ src/components/posts/post.jsx
@@ -4,7 +4,22 @@ export const Post = () => {
 
   return (
     <div className="post letra-clara">
-        
+          <div className='inner-post'>
+            <h2 id="title" className='title'>Title</h2>
+
+            <div className="description">
+              <p 
+                id="content" 
+                className='content'
+               /> 
+            </div>
+            <span> public </span>
+
+            <button type="submit">Post!</button>
+          </div>
+          <div className="enter-post">
+            <i class="fa-solid fa-angle-right"></i>
+          </div>
     </div>
   );
 }
