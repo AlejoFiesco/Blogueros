979c628d9a1e2880dbada5f960eaaad4efed218f
Yeferson Hernandez <yeyehernandez.0809gmail.com>
2022-09-25 22:10:53 -0500

    configurando rutas

--- src/App.js
+++ src/App.js
@@ -1,27 +1,12 @@
-import { Routes, Route } from "react-router-dom";
-import MotionRoute from "./layout/motion-route";
-import LogIn from "./components/login/log-in";
+import RouterApp from "./routers/Router-App";
 import "./App.css";
-import { Follows } from "./components/pages/follows/Follows";
 
 function App() {
-  const post = {
-    title: "Post Blogueros",
-    content: "Descripción del post Descripción del postDescripción del postDescripción del postDescripción del postDescripción del postDescripción del postDescripción del post",
-    visibility: "public",
-    comments: 3,
-    date: "23 Sep 2022",
-    userTag: "alejo123"
-  };
-
+ 
   return (
-    <Routes>
-      <Route path="/" element={<MotionRoute element="login"/>} />
-      <Route path="/post" element={<MotionRoute element="post" post={ post }/>} />
-      <Route path="/newpost" element={<MotionRoute element="newpost"/>} />
-      <Route index element={<LogIn />} />
-      <Route path="follows" element={<Follows />} />
-    </Routes>
+    <div>
+      <RouterApp />
+    </div>
   );
 }
 export default App;
---
+++ src/routers/Router-App.jsx
@@ -0,0 +1,28 @@
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import MotionRoute from "../layout/motion-route";
+import LogIn from "../components/login/log-in";
+
+import { DashboarRouter } from "./Dashboar-Router";
+
+
+function RouterApp() {
+ 
+
+  return (
+    <BrowserRouter>
+        <Routes>    
+            {/* rutas publicas */}
+            <Route path="/" element={<MotionRoute element="login"/>} />
+            <Route index element={<LogIn />} />
+
+            {/* rutas privadas */}
+
+            <Route path='/*' element={
+                <DashboarRouter />
+            }/>
+
+        </Routes>
+    </BrowserRouter>
+  );
+}
+export default RouterApp;
