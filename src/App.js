52061ca5fb56110d14e01f69e07ed83195dde0d1
adelquis-molinari <adelkis003@gmail.com>
2022-09-26 22:08:18 -0300

    created auth and context

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import LogIn from "./components/pages/login/log-in";
 import { Follows } from "./components/pages/follows/Follows";
 import { Post } from "./components/posts/post";
 import { NewPost } from "./components/posts/new-post";
+import { AuthProvider } from "./context/authContext";
 import "./App.css";
 
 function App() {
@@ -19,14 +20,16 @@ function App() {
   };
 
   return (
-    <Routes>
-      <Route path="/" element={<MotionRoute />}>
-        <Route index element={<LogIn />} />
-        <Route path="/post" element={<Post post={post} />} />
-        <Route path="/newpost" element={<NewPost />} />
-        <Route path="follows" element={<Follows />} />
-      </Route>
-    </Routes>
+    <AuthProvider>
+      <Routes>
+        <Route path="/" element={<MotionRoute />}>
+          <Route index element={<LogIn />} />
+          <Route path="/post" element={<Post post={post} />} />
+          <Route path="/newpost" element={<NewPost />} />
+          <Route path="follows" element={<Follows />} />
+        </Route>
+      </Routes>
+    </AuthProvider>
   );
 }
 export default App;
